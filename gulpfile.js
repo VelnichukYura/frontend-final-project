5d52bb21ba3ec6ce0d3bfb1c450d4eaf227dc60d
Alex Savchuk <alexsavchuk739291@gmail.com>
2016-03-26 05:58:53 +0200

    problem gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,37 +5,40 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    connect = require('gulp-connect'),
+    opn = require('opn'),
     jade = require('gulp-jade');
-
-
-gulp.task('default', function() {
-    // place code for your default task here
+//conect
+gulp.task('connect', function() {
+    connect.server({
+        root: 'src',
+        livereload: true,
+        port:8888
+    });
+    opn('http://localhost:8888')
 });
-
 //jade
 gulp.task('jade', function() {
     var YOUR_LOCALS = {};
 
-    gulp.src('./src/jade/*.jade')
+    gulp.src('src/jade/*.jade')
         .pipe(jade({
             locals: YOUR_LOCALS
         }))
+        .pipe(connect.reload())
         .pipe(gulp.dest('./dist/'))
 });
-
 //sass
 gulp.task('sass', function () {
     return gulp.src('src/scss/style.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass())
+        .pipe(connect.reload())
         .pipe(gulp.dest('dist/css'));
 });
-
 //watch
 gulp.task('watch',function() {
+    gulp.watch('src/jade/*.jade', ['jade'])
     gulp.watch('src/scss/style.scss', ['sass'])
-    gulp.watch('./src/jade/*.jade', ['jade'])
-
 });
-
 //default task
-gulp.task('default', ['jade', 'sass','watch']);
+gulp.task('default', ['connect','watch']);
