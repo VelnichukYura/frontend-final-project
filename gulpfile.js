6d1a03fbd59a38abada7baf1b8220222faa99205
bagmetv <bagmet.v@gmail.com>
2016-03-28 19:03:53 +0300

    updates

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var jade = require('gulp-jade');
+var git = require('gulp-git');
+
+// SASS TASK
+gulp.task('sass', function () {
+    return gulp.src("src/scss/style.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("dist/css"));
+});
+
+// JADE TASK
+gulp.task('jade', function () {
+    gulp.src('./src/jade/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./dist/'));
+});
+
+// GIT COMMIT TASK
+gulp.task('commit', function () {
+    git.exec({args: 'add -A'}, function (err, stdout) {
+        git.exec({args: 'diff --name-status --cached --raw'}, function (err, stdout) {
+            var message = stdout.replace(/\t/g, " - ").replace(/\n/g, ";\n");
+            git.exec({args: 'commit -m "' + message + '"'}, function (err, stdout) {
+                console.log(err);
+                console.log(stdout);
+            });
+        });
+    });
+});
+
+// GIT PULL TASK
+gulp.task('pull', function () {
+    git.exec({args: 'pull'}, function (err, stdout) {
+        console.log(err);
+        console.log(stdout);
+    });
+});
+
+// GIT PUSH TASK
+gulp.task('push', function () {
+    git.exec({args: 'push'}, function (err, stdout) {
+        console.log(err);
+        console.log(stdout);
+    });
+});
+
+
+gulp.task('watcher', function () {
+    //sass
+    //jade
+});
+
+
+gulp.task('default', ['watcher']);
