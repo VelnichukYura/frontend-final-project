4ccb2720e50a0c557b55b537b73c7d08b37a7300
bagmetv <bagmet.v@gmail.com>
2016-03-30 18:09:52 +0300

    ongoing updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,36 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var jade = require('gulp-jade');
-var git = require('gulp-git');
 var browserSync = require('browser-sync').create();
 
+// SERVER + WATCH
+gulp.task('serve', ['sass', 'jade'], function() {
+
+  browserSync.init({
+    server: "./dist"
+  });
+
+  gulp.watch("src/scss/**/*.scss", ['sass']);
+  gulp.watch('src/**/*.jade', ['jade']);
+  gulp.watch("dist/*.html").on('change', browserSync.reload);
+  });
 
 // SASS TASK
 gulp.task('sass', function(){
-  return.gulp.src('src/scss/style.scss')
+  return gulp.src('src/scss/*.scss')
       .pipe(sass())
-      .pipe(gulp.dest('app/css'))
-      .pipe(browserSync.reload({
-        stream: true
-      }))
+      .pipe(gulp.dest('dist/css'))
+      .pipe(browserSync.stream());
 });
-
 
 // JADE TASK
 gulp.task('jade', function () {
- return. gulp.src('./src/jade/*.jade')
+ return gulp.src('src/jade/*.jade')
       .pipe(jade({
         pretty: true
       }))
-      .pipe(gulp.dest('./dist/'));
+      .pipe(gulp.dest('dist/'));
 });
 
-//BROWSER-SYNC TASK
-gulp.task('browserSync', function(){
-  browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-  })
-});
-
-gulp.task('watch', ['browserSync', 'sass'], function(){
-  gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('src/jade/**/*.jade'), ['jade']);
-  gulp.watch('dist/*.html', browserSync.reload);
-});
+//SERVE
+gulp.task('default', ['serve']);
