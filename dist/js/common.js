010472438ad802658fcae723b57e056c8b486c9f
Andrew Ilyushchenko <andrew.ilyushchenko@gmail.com>
2016-04-11 15:29:38 +0300

    without carousel products

--- dist/js/common.js
+++ dist/js/common.js
@@ -7,28 +7,38 @@ $(function() {
             var element = $(this),
                 width = element.innerWidth();
 
-            if (width > 1200) {
-                width = width / 1;
-            } else if (width < 600) {
-                width = width / 1 ;
-            }
-
+            // This shows 1 item at a time.
+            // Divide `width` to the number of items you want to display,
+            // eg. `width = width / 3` to display 3 items at a time.
             element.jcarousel('items').css('width', width + 'px');
         })
-
         .jcarousel({
-            // Your configurations options
+            wrap: 'circular'
         });
 
     // Инициализация прокрутки слайдера
 
-    $('.jcarousel-prev').jcarouselControl({
-        target: '-=1'
-    });
+    $('.jcarousel-control-prev')
+        .on('jcarouselcontrol:active', function () {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function () {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            target: '-=1'
+        });
 
-    $('.jcarousel-next').jcarouselControl({
-        target: '+=1'
-    });
+    $('.jcarousel-control-next')
+        .on('jcarouselcontrol:active', function () {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function () {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            target: '+=1'
+        });
 
     // Инициализация пагинации слайдера
 
