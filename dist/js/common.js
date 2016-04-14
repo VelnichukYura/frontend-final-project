1ced512dd9e82563469931928e2e92a38d0e8fad
Andrew Ilyushchenko <andrew.ilyushchenko@gmail.com>
2016-04-14 18:16:55 +0300

    finish

--- dist/js/common.js
+++ dist/js/common.js
@@ -44,7 +44,7 @@ $(function() {
 
     $('.jcarousel-pagination')
 
-        // Триггер класса active
+    // Триггер класса active
 
         .on('jcarouselpagination:active', 'a', function() {
             $(this).addClass('active');
@@ -118,24 +118,18 @@ function productsCarousel(id) {
     var jcarousel = $('#' + id);
 
     jcarousel
-        .on('jcarousel:reload jcarousel:create', function () {
-            var carousel = $(this),
-                width = carousel.innerWidth();
-
-            if (width >= 600) {
-                width = width / 3;
-            } else if (width >= 350) {
-                width = width / 2;
-            }
-
-            carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
-        })
         .jcarousel({
             wrap: 'circular'
         });
 
     jcarousel.jcarouselAutoscroll({
-        interval: 2000
+        interval: 4000
+    });
+
+    jcarousel.hover(function() {
+        jcarousel.jcarouselAutoscroll('stop');
+    }, function() {
+        jcarousel.jcarouselAutoscroll('start');
     });
 
     $('#-prev-' + id)
